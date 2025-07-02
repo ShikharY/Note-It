a96ce19b13ff3efba5e6667be68f30740f975e49
Elahe Mahmoodpour <elahe.mahmoodpour@gmail.com>
2025-07-02 23:57:02 +0200

    refactor: add some lines to webpack.config.js and Package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: {
-    popup: './src/popup.js',
-    graphView: './src/graphView.js'
+    popup: "./src/popup.js",
+    graphView: "./src/graphView.js",
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
-    clean: true
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    clean: true,
   },
   module: {
     rules: [
@@ -18,36 +18,35 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader']
-      }
-    ]
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/popup.html',
-      filename: 'popup.html',
-      chunks: ['popup']
+      template: "./src/popup.html",
+      filename: "popup.html",
+      chunks: ["popup"],
     }),
     new HtmlWebpackPlugin({
-      template: './src/graphView.html',
-      filename: 'graphView.html',
-      chunks: ['graphView']
+      template: "./src/graphView.html",
+      filename: "graphView.html",
+      chunks: ["graphView"],
     }),
     new CopyWebpackPlugin({
-      patterns: [
-        { from: 'manifest.json', to: 'manifest.json' }
-      ]
-    })
+      patterns: [{ from: "manifest.json", to: "manifest.json" }],
+    }),
   ],
   resolve: {
-    extensions: ['.js', '.jsx']
-  }
-}; 
+    extensions: [".js", ".jsx"],
+  },
+  devtool: "cheap-module-source-map",
+};
